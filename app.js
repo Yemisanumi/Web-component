ac3bba9190d53a097410bcaa4ecad712ed7eef11
Oluwayemi Sanumi <oluwayemisanumi@gmail.com>
2024-02-20 13:42:59 -0800

    added some styling

--- app.js
+++ app.js
@@ -4,21 +4,36 @@ class NavLists extends HTMLElement {
       const shadowRoot = this.attachShadow({ mode: 'open' });
       shadowRoot.innerHTML = `
         <style>
-          /* CSS styles for the component */
-          :host {
-            display: inline-block;
-            padding: 8px 16px;
-            cursor: pointer;
-          }
-  
-          :host(:hover) {
-            background-color: #f1f1f1;
-          }
+        ul {
+          display: flex;
+          width: 500px;
+          justify-content: space-between;
+          
+          list-style: none;
+         
+      
+        } 
+      li a {
+          color: var(--Gray-1, #333);
+          font-family: 'Roboto' sans-serif;
+          font-size: 20px;
+          font-style: normal;
+          font-weight: 500;
+          text-decoration: none;
+          list-style: none;
+          justify-content: space-between;
+        }
         </style>
         <slot></slot>
+        <ul class="lists">
+                    <li><a href="">About</a></li>
+                    <li><a href="">How it works</a></li>
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Registration</a></li>
+                </ul>
       `;
     }
-  
+
     connectedCallback() {
         this.addEventListener('click', () => {
           alert('Click');
